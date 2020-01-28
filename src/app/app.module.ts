204ebb4e9747e4a4e82c1760941ebc855b6cf672
Cristian Garibay <cristian.garibay@infosys.com>
2020-01-28 10:03:17 -0600

    Minimalist landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { NavigatorComponent } from './navigator/navigator.component';
+
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+import { MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigatorComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +39,10 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
